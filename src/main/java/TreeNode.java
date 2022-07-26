52359040722b3d5fb195db5d13621944fdefff14
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-07-26 12:26:51 -0600

    Se crearon los getter y setters de la clase Nodo

--- src/main/java/TreeNode.java
+++ src/main/java/TreeNode.java
@@ -1,23 +1,38 @@
 public class TreeNode {
+    private int data;
+    private TreeNode rightNode;
+    private TreeNode leftNode;
 
-    TreeNode rightNode;
-    int data;
-    TreeNode leftNode;
-
-
-    public TreeNode(int dataNode)
-    {
+    public TreeNode(int dataNode) {
         data = dataNode;
         rightNode = leftNode = null;
     }
 
-
-
-    public void insert(int value)
-    {
-
+    public void insert(int value) {
 
     }
 
+    public int getData() {
+        return data;
+    }
 
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
 }
