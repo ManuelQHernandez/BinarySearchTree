3108f32935e3ab9fa62b30a653d172c75d3a6569
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-07-26 15:25:40 -0600

    Se agrego el metodo para imprimir arbol a la clase Arbol

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-
         Tree tree = new Tree();
 
         tree.insertNode(5);
@@ -14,62 +13,7 @@ public class Main {
         tree.insertNode(2);
         tree.insertNode(12);
 
-
-        printBinaryTree(tree.getRoot());
-    }
-
-    public static void printBinaryTree(TreeNode root) {
-        LinkedList<TreeNode> treeLevel = new LinkedList<TreeNode>();
-        treeLevel.add(root);
-        LinkedList<TreeNode> temp = new LinkedList<TreeNode>();
-        int counter = 0;
-        int height = heightOfTree(root)-1;
-
-        double numberOfElements = (Math.pow(2 , (height + 1)) - 1);
-
-        while (counter <= height) {
-            TreeNode removed = treeLevel.removeFirst();
-            if (temp.isEmpty()) {
-                printSpace(numberOfElements / Math.pow(2 , counter + 1), removed);
-            } else {
-                printSpace(numberOfElements / Math.pow(2 , counter), removed);
-            }
-            if (removed == null) {
-                temp.add(null);
-                temp.add(null);
-            } else {
-                temp.add(removed.getLeftNode());
-                temp.add(removed.getRightNode());
-            }
-
-            if (treeLevel.isEmpty()) {
-                System.out.println("");
-                System.out.println("");
-                treeLevel = temp;
-                temp = new LinkedList<>();
-                counter++;
-            }
-
-        }
-    }
-
-    public static void printSpace(double n, TreeNode removed){
-        for(;n>0;n--) {
-            System.out.print("\t");
-        }
-        if(removed == null){
-            System.out.print(" ");
-        }
-        else {
-            System.out.print(removed.getData());
-        }
-    }
-
-    public static int heightOfTree(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        return 1+ Math.max(heightOfTree(root.getLeftNode()),heightOfTree(root.getRightNode()));
+        tree.printTree();
     }
 
 }
--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,7 +1,7 @@
+import java.util.LinkedList;
+
 public class Tree {
-
     private TreeNode root;
-
 
     public Tree() {
         root = null;
@@ -17,5 +17,53 @@ public class Tree {
         } else {
             root.insert(value);
         }
+    }
+
+    public void printTree() {
+        int counter = 0;
+        int height = heightOfTree(root)-1;
+        LinkedList<TreeNode> treeLevel = new LinkedList<TreeNode>();
+        treeLevel.add(root);
+        LinkedList<TreeNode> temp = new LinkedList<TreeNode>();
+
+        double numberOfElements = (Math.pow(2 , (height + 1)) - 1);
+
+        while (counter <= height) {
+            TreeNode removed = treeLevel.removeFirst();
+            if (temp.isEmpty())
+                printSpace(numberOfElements / Math.pow(2 , counter + 1), removed);
+            else
+                printSpace(numberOfElements / Math.pow(2 , counter), removed);
+
+            if (removed == null) {
+                temp.add(null);
+                temp.add(null);
+            } else {
+                temp.add(removed.getLeftNode());
+                temp.add(removed.getRightNode());
+            }
+
+            if (treeLevel.isEmpty()) {
+                System.out.println("\n");
+                treeLevel = temp;
+                temp = new LinkedList<>();
+                counter++;
+            }
+
+        }
+    }
+
+    private void printSpace(double n, TreeNode removed){
+        for(;n>0;n--)
+            System.out.print("\t");
+        if(removed == null)
+            System.out.print(" ");
+        else
+            System.out.print(removed.getData());
+    }
+
+    private int heightOfTree(TreeNode root){
+        if(root==null) return 0;
+        return 1+ Math.max(heightOfTree(root.getLeftNode()),heightOfTree(root.getRightNode()));
     }
 }
