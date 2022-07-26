60004c5b8c6ec03c6eef23b70b323292f0f8c26c
Gabriela <gabi16.hernandez@gmail.com>
2022-07-26 15:24:33 -0600

    Se modificaron los test de Tree

--- src/test/java/TreeTest.java
+++ src/test/java/TreeTest.java
@@ -3,30 +3,28 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TreeTest {
+    TreeNode root;
     Tree tree;
-    int max;
     int value;
-    int children;
 
     @Before
     public void init() {
         tree = new Tree();
-        max = 3;
-        children = 2;
-        //value = -1;
-        value = 2;
+        root = null;
+        value = 5;
     }
 
     @Test
-    public void testInsertNode() {
+    public void shouldInsertNode() {
+        root = new TreeNode(value);
         tree.insertNode(value);
+        assertNotNull(root);
+    }
 
-        if (value >= max) {
-            fail("El nodo no puede ser colocado porque no existen nodos padre");
-        } else if (value == children) {
-            fail("El nodo no pudo ser insertado porque ya hay 2 hijos en esa rama");
-        } else {
-            System.out.println("Todo salió perfecto");
-        }
+    @Test
+    public void shouldInsertValueOnRoot() {
+        root = new TreeNode(value);
+        root.insert(value);
+        assertNotNull(root);
     }
 }
